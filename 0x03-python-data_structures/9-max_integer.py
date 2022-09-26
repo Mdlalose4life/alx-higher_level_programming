33146e5bceaf59ab9d58c54904eee5355a792941
Mdlalose4life <blessing1059@gmail.com>
2022-09-27 00:29:38 +0200

    Done

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    for i in my_list:
+        big_num = num1 = num2 = my_list[i]
+        if len(my_list) == 0:
+            return None
+        if num1 > num2:
+            num1 = big_num
+        elif num2 > num1:
+            num2 = big_num 
+            return big_num
