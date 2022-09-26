b7734e19ba1f47ec73291b4a0ab83cca41acc2dd
Mdlalose4life <blessing1059@gmail.com>
2022-09-26 19:56:05 +0200

    Done

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    for i in my_string:
+        new_string = (my_string.translate({ord(i): None for i in 'c'}))
+        new_string = (new_string.translate({ord(i): None for i in 'C'}))
+            return new_string
