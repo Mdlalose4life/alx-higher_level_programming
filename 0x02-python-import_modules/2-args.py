4cb7c0056caf82029889f092837bf529a1a81287
Mdlalose4life <blessing1059@gmail.com>
2022-09-28 12:26:47 +0200

    third commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    arguments = len(argv)
+    i = 1
+    if arguments == 0:
+        print("{:d} arguments.".format(arguments))
+    elif arguments == 1:
+        print("{:d} arguments.".format(arguments))
+        print("{:d}: {:s}".format(i,argv))
+    else:
+        print("{:d} arguments.".format(arguments))
+        while i <= arguments:
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i =+ 1
