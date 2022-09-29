b8a5e3b979285cc3bd67a792893761e58403b282
Mdlalose4life <blessing1059@gmail.com>
2022-09-29 05:15:18 +0200

    Done

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {key: value * 2 for key, value in a_dictionary.iterms()}
+    return {i: (a_dictionary[i] * 2) for i in a_dictionary}
