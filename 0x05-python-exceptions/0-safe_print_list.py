82bb66bf587869331e34c22cc32b728e8228898e
Mdlalose4life <blessing1059@gmail.com>
2022-10-11 03:30:56 +0200

    project push

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    sum = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            sum += 1
+        except IndexError:
+            break
+    print()
+    return sum
