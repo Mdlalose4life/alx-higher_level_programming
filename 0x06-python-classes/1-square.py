77beeac5ab759e542e3f89c8b79c88b2d2aa42f3
Mdlalose4life <blessing1059@gmail.com>
2022-10-12 04:50:39 +0200

    Project push

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,16 @@
 
 
 class Square:
+    """Represent a square
+    Attribute:
+        __size(int): size of a side of the square
+    """
     def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size (int): size of a side of the square
+
+            Return: None
+        """
         self.__size = size
