92c328dd77bce2d2b816e5dcfb9412b0a50b92f6
Mdlalose4life <blessing1059@gmail.com>
2022-10-12 05:41:33 +0200

    projec

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,31 +4,27 @@
 
 class Square:
     """Represents a square
-
-
-    Attribute:
-        __size(int): size of a side of the square
+    Attributes:
+        __size (int): size of a side of the square
     """
     def __init__(self, size=0):
         """initializes the square
         Args:
             size (int): size of a side of the square
-
-            Return:
-            	None
+        Returns:
+            None
         """
         if type(size) is not int:
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         else:
             if size < 0:
-                raise ValueError('size must be >= 0')
+                raise ValueError("size must be >= 0")
             else:
                 self.__size = size
 
-        def area(self):
-            """Calulates the square's area
-
-            returns:
-                The are of a square
-            """
-            return (self.__size) ** 2
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
