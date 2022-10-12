3d016894a42231c97f648f4032850cde4ad61bbf
Mdlalose4life <blessing1059@gmail.com>
2022-10-12 05:19:10 +0200

    Project push

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represent a square
+    Attribute:
+        __size(int): size of a side of the square
+    """
+    def __init__(self, size):
+        """
+        Args:
+            size (int): size of a side of the square
+
+            Return: None
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
