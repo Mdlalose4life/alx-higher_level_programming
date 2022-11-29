3f82efe5c868e4e77e1fc4e9af237488e6728eb8
Mdlalose4life <blessing1059@gmail.com>
2022-11-30 01:35:43 +0200

    Number 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,4 @@
 -- Create table
 --Scrip that creates a table called first_table in the current database in your MySQL server.
 
-create table if not exists first_table(
-	id INT,
-	name VARCHAR(256)
-);
+create table if not exists first_table(id INT, name VARCHAR(256));
