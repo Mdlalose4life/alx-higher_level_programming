04a5d4912ea83494159881dca698a3d2ceb98b26
Mdlalose4life <blessing1059@gmail.com>
2022-11-30 01:50:38 +0200

    Project Push

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,4 +2,4 @@
 -- Script that inserts a new row in the table first_table in your MySQL server.
 
 INSERT INTO(id, name)
-VALUES(89, "Best School")
+VALUES(89, "Best School");
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Count 89
+-- Script that displays the number of records with id = 89 in the table first_table.
+
+SELECT COUNT(id = 89)
+FROM first_table;
