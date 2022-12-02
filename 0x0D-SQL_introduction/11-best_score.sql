1f30a60fe6e2fcbb4e0bfd2f52722f4235414cf0
Mdlalose4life <blessing1059@gmail.com>
2022-12-02 03:05:32 +0200

    Project Push

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- list the records in second_table where.
+-- Score is greater or equal to 10
+SELECT *FROM second_table WHERE score >= 10;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Updatte Bob score to be 10.
+-- Must use no Bob's id Value but Bob's name.
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
