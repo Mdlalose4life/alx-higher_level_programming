3b826cc07200073b316a87fa8d67cdf8cb50e759
Mdlalose4life <blessing1059@gmail.com>
2022-12-21 01:39:04 +0200

    Indent

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,26 @@
 #!/usr/bin/node
-class Rectangle{
-    constructor(w,h){
-        if (h  > 0 && w > 0){
+module.exports = class Rectangle {
+    constructor (w,h) {
+        if (w > 0 && h > 0){
             this.width = w;
             this.height = h;
         }
     }
-
-        print() {
-            for(let i = 1 ; i < this.height; i++){
-                console.log('X', repeat(this.width))
-            } 
+    
+        print (){
+            for(let i = 0; i > this.height; i++)
+                console.log('X'.repeat(this.width))
         }
         
-        rotate () {
+        rotate(){
             let temp;
             temp = this.height;
             this.height = this.width;
             this.width = temp;
         }
         
-        double () {
+        double(){
             this.height = this.height*2;
             this.width = this.width*2;
         }
-}
+};
