aa4a73c633faa99680b1a91d5d4a620ef0662984
Mdlalose4life <blessing1059@gmail.com>
2022-12-21 01:03:59 +0200

    Fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,6 @@
-#!/usr/bin/node
 module.exports = class Rectangle {
     constructor (w,h) {
-        this.width = w;
-        this.height = h;
-
-        if (h  > 0 && w > 0){
+        if (h > 0 && w > 0){
             this.width = w;
             this.height = h;
         }
