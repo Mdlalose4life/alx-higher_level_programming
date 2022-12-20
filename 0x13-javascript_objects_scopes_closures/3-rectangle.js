6e3e31d622a650486aa74b3ec0dbd2006dd02bf5
Mdlalose4life <blessing1059@gmail.com>
2022-12-21 01:09:37 +0200

    Project Check

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-    constructor (w,h) {
-        if (h > 0 && w > 0){
+module.exports = class Rectangle{
+    constructor(w,h){
+        this.width = w;
+        this.height = h;
+
+        if (h  <= 0 && w <= 0){
             this.width = w;
             this.height = h;
+        }
+        print();
+        {
+            let i = 1;
+            for(; i > this.height; i++){
+                console.log('X', repeat(this.width))
+            } 
         }
     }
 }
