2ad60c21effcd1c48eb95122da7a126d3ea270f4
Mdlalose4life <blessing1059@gmail.com>
2022-12-21 00:59:55 +0200

    Check

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+    constructor (w,h) {
+        this.width = w;
+        this.height = h;
+
+        if (h  > 0 && w > 0){
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
