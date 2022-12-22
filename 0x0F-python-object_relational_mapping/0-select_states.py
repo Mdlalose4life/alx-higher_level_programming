cb8d046eaafa0bb1d26560e3d94f1080169d5c42
Mdalose4life <blessing1059@gmail.com>
2022-12-23 01:41:39 +0200

    Project Check

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,19 @@
-import mysql.connector
+#!/usr/bin/python3
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
 
-mydb = mysql.connector.connect(host="localhost", 
-user="sqluser", 
-passwd="password", 
-db=" hbtn_0e_0_usa")
-
-print(mydb)
-
+if __name__=='__main__':
+    mydb = MySQLdb.connect(host="localhost", 
+                           user=argv[1], 
+                           passwd=argv[2], 
+                           db=argv[3])
 mycursor = mydb.cursor()
-
 mycursor.execute("SELECT *FROM states")
-for x in mycursor:
+mydb = mycursor.fetchall()
+for x in mydb:
     print(x)
+mycursor.close()
+mydb.close()
