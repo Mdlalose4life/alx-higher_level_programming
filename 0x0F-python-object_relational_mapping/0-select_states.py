0cc8a8c693b793d7a241fa0753ad9379a0a2b4cf
Mdalose4life <blessing1059@gmail.com>
2022-12-23 01:51:56 +0200

    Fixing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,10 +11,10 @@ if __name__=='__main__':
                            user=argv[1], 
                            passwd=argv[2], 
                            db=argv[3])
-mycursor = mydb.cursor()
-mycursor.execute("SELECT *FROM states")
-mydb = mycursor.fetchall()
-for x in mydb:
-    print(x)
-mycursor.close()
-mydb.close()
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT *FROM states")
+    mydb = mycursor.fetchall()
+    for x in mydb:
+        print(x)
+    mycursor.close()
+    mydb.close()
