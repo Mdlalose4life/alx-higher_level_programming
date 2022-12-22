5feac2b125468d1c272707b9a89540036bc99990
Mdalose4life <blessing1059@gmail.com>
2022-12-23 01:55:38 +0200

    fix order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,10 +11,10 @@ if __name__=='__main__':
                            user=argv[1], 
                            passwd=argv[2], 
                            db=argv[3])
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT *FROM states")
-    mydb = mycursor.fetchall()
+    cursor = mydb.cursor()
+    cursor.execute("SELECT *FROM states ORDER BY ASC")
+    mydb = cursor.fetchall()
     for x in mydb:
         print(x)
-    mycursor.close()
+    cursor.close()
     mydb.close()
