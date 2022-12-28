3c07849220352e599ee9eda503b2aeec084571ae
Mdlalose4life <blessing1058@gmail.com>
2022-12-28 04:00:19 +0200

    Correct Indentation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,27 @@
+
 #!/usr/bin/python3
 """
-a script that takes in an argument and displays
-all values in the states table of hbtn_0e_0_usa.
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
     Access to the database and find state that
     Matches the given name.
     """
     conn = MySQLdb.connect(host="localhost", port=3306,
-    user=argv[1], passwd=argv[2], db=argv[3])
+                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    
+
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY\
-        '{:s}' ORDER BY states.id ASC".format(argv[4]))
-    
-    for x in cursor.fetchall():
+        "SELECT * FROM states WHERE name LIKE BINARY '{:s}' ORDER BY \
+                        states.id ASC".format(argv[4]))
+
+    for x in cur.fetchall():
         print(x)
