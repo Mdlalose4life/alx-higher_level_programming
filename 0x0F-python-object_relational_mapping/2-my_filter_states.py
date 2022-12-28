1cbcd19b452f42b86b2ddc592c7168cfec645df7
Mdlalose4life <blessing1058@gmail.com>
2022-12-28 03:53:35 +0200

    indentation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,17 +8,17 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
- """
-Access to the database and find state that
-Matches the given name.
-"""
-conn = MySQLdb.connect(host="localhost", port=3306,
-user=argv[1], passwd=argv[2], db=argv[3])
-cur = conn.cursor()
-
-cur.execute(
-    "SELECT * FROM states WHERE name LIKE BINARY\
-             '{:s}' ORDER BY states.id ASC".format(argv[4]))
-
-for x in cursor.fetchall():
-    print(x)
+    """
+    Access to the database and find state that
+    Matches the given name.
+    """
+    conn = MySQLdb.connect(host="localhost", port=3306,
+    user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY\
+        '{:s}' ORDER BY states.id ASC".format(argv[4]))
+    
+    for x in cursor.fetchall():
+        print(x)
