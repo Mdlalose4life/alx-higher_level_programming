42b7aabc2ea4a28ad4b4f90690fe4865483f47b6
Mdlalose4life <blessing1058@gmail.com>
2022-12-28 03:50:50 +0200

    Indent

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,19 +7,18 @@ all values in the states table of hbtn_0e_0_usa.
 import MySQLdb
 from sys import argv
 
-"""
+if __name__ == "__main__":
+ """
 Access to the database and find state that
 Matches the given name.
 """
+conn = MySQLdb.connect(host="localhost", port=3306,
+user=argv[1], passwd=argv[2], db=argv[3])
+cur = conn.cursor()
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306,
-    user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
+cur.execute(
+    "SELECT * FROM states WHERE name LIKE BINARY\
+             '{:s}' ORDER BY states.id ASC".format(argv[4]))
 
-    cur.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY"\
-            " '{:s}' ORDER BY states.id ASC".format(argv[4]))
-
-    for x in cursor.fetchall():
-        print(x)
+for x in cursor.fetchall():
+    print(x)
