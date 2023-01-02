fab0e00376b61bd1fb2b7ffa7a191b9ee7cc91d8
Mdlalose4life <blessing1058@gmail.com>
2023-01-02 02:02:49 +0200

    Project Check

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,4 +6,4 @@ SELECT tv_shows.`title`,
 		FROM tv_shows
 		INNER JOIN tv_show_genres.`genre_id`
 		ON tv_shows.`id` = tv_show_genres.`show_id`
-ORDER BY tv_shows.`title`, tv_show_genres.`genre_id
+ORDER BY tv_shows.`title`, tv_show_genres.`genre_id;
