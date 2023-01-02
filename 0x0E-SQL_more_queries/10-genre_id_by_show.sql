8ef3821ad704da99b3d9ceb3f11869e484c688cf
Mdlalose4life <blessing1058@gmail.com>
2023-01-02 03:44:18 +0200

    Importing the data

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,10 @@
 -- Import the database
 -- Import the database dump from hbtn_0d_tvshows into MySQL server.
+-- Display tv_shows.title - tv_show_genres.genre_id in asc order
 MySQL < hbtn_0d_tvshows
 SELECT tv_shows.`title`,
-	tv_show_genres.`genre_id`
-		FROM tv_shows
+tv_show_genres.`genre_id`
+	FROM tv_shows
 		INNER JOIN tv_show_genres.`genre_id`
 		ON tv_shows.`id` = tv_show_genres.`show_id`
-ORDER BY tv_shows.`title`, tv_show_genres.`genre_id;
+ORDER BY ASC tv_shows.`title`, tv_show_genres.`genre_id;
