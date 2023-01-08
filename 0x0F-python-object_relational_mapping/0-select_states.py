0a28b1db0127fc40fcc44e2f31f72c408a9318de
Mdlalose@1104 <blessing1059@gmail.com>
2023-01-08 15:57:02 +0200

    access dot

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,21 +5,21 @@ database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    
+"""
+Access to the database and get the states
+from the database.
+"""
+
+
 if __name__ == "__main__":
 
     con = MySQLdb.connect(
         host="localhost",port=3306, user=sys.argv[1], passwd=sys.argv[2],
         db=sys.argv[3], charset="utf8")
     cu = con.cursor()
-    cu.execute("SELECT * FROM states")
+    cu.execute("SELECT * FROM states ORDER BY states.id")
     for row in cu.fetchall():
         print(row)
     cu.close()
