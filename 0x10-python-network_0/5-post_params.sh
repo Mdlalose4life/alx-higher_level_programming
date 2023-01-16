8ff477ad3059ad0a3661acec7ccca3495d24ec46
Mdlalose4life <blessing1059@gmail.com>
2023-01-17 00:10:31 +0200

    .

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Pots the request to URL then display result body
-curl -s -X POST -d "X-School-User-ID: 98" "$1"
+curl -s -X POST -d "test@gmail.com.&subject= I will always be here for PLD" "$1"
