ab978d5deb8f3fa384e67ad5e5995a14dc426bf7
Mdlalose4life <blessing1059@gmail.com>
2023-01-17 23:52:25 +0200

    runs okay

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,5 +10,5 @@ from urllib import request
 import sys
 
 if __name__ == "__main__":
-    with request.urlopen(argv[0]) as response:
+    with request.urlopen(sys.argv[1]) as response:
     	print(response.getheader("X-Request-Id"))
