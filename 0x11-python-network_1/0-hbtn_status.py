8603c84a107432a4ca6fae77c9f864a82e66dd54
Mdlalose4life <blessing1059@gmail.com>
2023-01-17 15:05:21 +0200

    formatted strings

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,11 @@
 import urllib.request
 """Pythone script that fetches a URL"""
 
+
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
         html = resp.read()
-        print(html)
+        print("body responce:")
+        print("\t type: {}".format(type(html)))
+        print("\t contant {}".format(html))
+        print("\t utf-8 content {}".format(html.decode('utf-8')))
