ea1233f4ec477b339d38f61a740267394c39f05e
Mdlalose4life <blessing1059@gmail.com>
2023-01-17 22:01:21 +0200

    Task_1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,9 +10,5 @@ import urllib.request
 from sys
 
 if __name__ == "__main__":
-
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as resp:
-        print(dick(resp.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.getheader("X-Request-Id"))
