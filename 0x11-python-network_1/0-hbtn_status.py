aea557bb88d5fda9b3330958308958d6cb7ef83a
Mdlalose4life <blessing1059@gmail.com>
2023-01-17 15:12:43 +0200

    Capitilize B

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import urllib.request
-"""Pythone script that fetches a URL"""
+"""Python script that fetches a URL"""
 
 
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
         html = resp.read()
-        print("body responce:")
+        print("Body responce:")
         print("\t type: {}".format(type(html)))
         print("\t contant: {}".format(html))
         print("\t utf-8 content: {}".format(html.decode('utf-8')))
