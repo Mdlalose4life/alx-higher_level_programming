5d365dba959607723ccc89a96cc44289a133a9f4
Mdlalose4life <blessing1059@gmail.com>
2023-01-19 23:53:10 +0200

    Edit the print funtion

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,8 +7,7 @@ import requests
 
 
 if __name__ == "__main__":
-    req = requests.get('ttps://alx-intranet.hbtn.io/status')
+    req = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t -type: {}'.format(type(req)))
-    print('\t -content: {}'.format(req))
-    
+    print('\t-type: {}'.format(type(req.text)))
+    print('\t-content: {}'.format((req.text)))
