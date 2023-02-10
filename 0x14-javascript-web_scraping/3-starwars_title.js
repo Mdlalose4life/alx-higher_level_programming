e25ab444e40432cd475beab35179881cc43f8237
Mdlalose4life <blessing1059@gmail.com>
2023-02-10 03:38:22 -0800

    the colon

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/:id';
+const url = 'https://swapi-api.alx-tools.com/api/films/:';
 const movie_Id = process.argv[2];
 
 request(url + movie_Id, function(error, response, body){
