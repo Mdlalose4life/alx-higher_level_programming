2e1e4371c3b6b7a156c0f67186f21e16acb161a9
Mdlalose4life <blessing1059@gmail.com>
2023-02-10 03:51:10 -0800

    the movie Id

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/:';
+const request = require(request)
 const movie_Id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/ + movie_Id';
 
 request(url + movie_Id, function(error, response, body){
 	if(error) {
