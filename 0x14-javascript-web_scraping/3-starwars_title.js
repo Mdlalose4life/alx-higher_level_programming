ee5f474bedcd62aa958212fade74a8dd0c1feeac
Mdlalose4life <blessing1059@gmail.com>
2023-02-10 03:32:17 -0800

    Starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,14 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
-const request = require('request')
-response = request.get('https://swapi-api.alx-tools.com/api/films/:id')
-console.log(response)
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/:id';
+const movie_Id = process.argv[2];
+
+request(url + movie_Id, function(error, response, body){
+	if(error) {
+		console.log(error);
+	}
+	else{
+		console.log(JSON.parse(body).title);
+	}
+});
