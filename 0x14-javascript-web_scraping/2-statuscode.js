ac6a8cfa1eeba9e21ac6e8a9a9f7344fac8b9701
Mdlalose4life <blessing1059@gmail.com>
2023-02-11 02:02:08 +0200

    "colons"

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,9 @@ const url = process.argv[2];
 
 
 request.get(url, (error, response) => {
-	if (error){
-		console.log(error);
-	}else
-	console.log('code: ${response.statusCode}');
+  if (error){
+	console.log(error);
+  }else{
+	console.log(`code: ${response.statusCode}`);
+  }
 });
