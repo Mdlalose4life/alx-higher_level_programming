cbba35dd271cb4566f5de9d471fa554f7ab0cc4c
Mdlalose4life <blessing1059@gmail.com>
2023-02-11 04:44:13 +0200

    Quick fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
-const movie_id = process.argv[2]
-const url = `https://swapi-api.alx-tools.com/films/${movie_id}/`
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
     return;
@@ -13,14 +13,14 @@ request(url, (error, response, body) => {
 
   const data = JSON.parse(body);
   const characters = data.characters;
-    for (const character of characters) {
-      request(character, (error, response, body) => {
-        if (error){
-          console.log(error);
-          return;
-        }
-        const characterData = JSON.parse(body);
-        console.log(characterData.name);
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
     });
   }
 });
