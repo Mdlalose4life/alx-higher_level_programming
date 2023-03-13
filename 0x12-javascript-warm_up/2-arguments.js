669f6eea0f3ac18e39cf14c4e0a2db9bb948ccb5
Mdlalose4life <blessing1059@gmail.com>
2023-03-14 01:38:31 +0200

    Counting arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+/* A script that prints a message depending of the number of arguments passed */
+
 if (process.argv.length === 2) {
     console.log('No argument');
   } else if (process.argv.length === 3) {
