4d762f57eb0ad5fdd83997ea896ce46540c8236c
Mdlalose4life <blessing1059@gmail.com>
2023-03-14 01:51:31 +0200

    The easier method

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-const arg2 = process.argv[2];
-const arg3 = process.argv[3];
-console.log(arg2 + ' is ' + arg3);
+/* A script that prints two arguments passed to it,
+in the following format: “ is ”*/
+
+console.log(process.argv[2] + ' is ' + process.argv[3])
