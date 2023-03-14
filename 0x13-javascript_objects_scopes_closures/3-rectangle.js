778251f5c64cfc142e0ee998df7a9bfd580d5f69
Mdlalose4life <blessing1059@gmail.com>
2023-03-15 01:45:25 +0200

    The other way

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor(w,h){
-        if (w > 0 && h > 0) {
+    constructor(w, h) {
+        if (w > 0 && h > 0){
             this.width = w;
             this.height = h;
         }
     }
-    
-        print () {
-            for (let i = 0; i < this.height; i++)
-                console.log('X'.repeat(this.width));
+
+    // Instantiate the instance method print that prints the X of rectangles
+    print() {
+        for (let i = 0; i < this.height; i++){
+            console.log('X'.repeat(this.width));
         }
+    }
 };
