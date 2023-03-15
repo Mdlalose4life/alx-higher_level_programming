37f477e7b168c4c375d6813c3951265447494369
Mdlalose4life <blessing1059@gmail.com>
2023-03-15 02:07:18 +0200

    Edits

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,29 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w,h) {
+    constructor(w, h) {
         if (w > 0 && h > 0){
             this.width = w;
             this.height = h;
         }
     }
-    
-        print (){
-            for(let i = 0; i < this.height; i++)
-                console.log('X'.repeat(this.width))
+
+    // Instantiate the instance method print that prints the X of rectangles
+    print() {
+        for (let i = 0; i < this.height; i++){
+            console.log('X'.repeat(this.width));
         }
-        
-        rotate(){
-            let temp;
-            temp = this.height;
-            this.height = this.width;
-            this.width = temp;
-        }
-        
-        double(){
-            this.height = this.height*2;
-            this.width = this.width*2;
-        }
+    }
+
+    //Instantiating the instance method that swap the width and height of the rectangle
+    rotate(){
+        let temp = this.width;
+        this.width = this.height;
+        this.height = temp;
+    }
+
+    //Instantiating the instance method that doubles the size of hight and width
+    double(){
+        this.height = this.height*2;
+        this.width = this.width*2;
+    }
 };
