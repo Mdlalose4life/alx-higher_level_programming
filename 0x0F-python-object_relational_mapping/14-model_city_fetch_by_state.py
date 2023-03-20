0801b0d56b6ba5a8357e55dcd57b2a9f8212f73e
Mdlalose4life <blessing1059@gmail.com>
2023-03-20 03:22:25 +0200

    Connecting the engine

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     from the database.
     """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    engine = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create_engine(db_url)
+    engine = create_engine(engine)
     Session = sessionmaker(bind=engine)
 
     session = Session()
