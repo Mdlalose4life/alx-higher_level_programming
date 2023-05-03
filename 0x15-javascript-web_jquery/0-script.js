a2c376757d51749f2e69f39b8a9d7ff859ecbe09
Mdlalose4life <blessing1059@gmail.com>
2023-05-04 01:56:01 +0200

    The files

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// Select the element to change the color of
+const header = document.querySelector('header');
+//uUpdates the color of the header element
+header.style.color = '#FF0000'
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+// Script that changes the color of header
+$(function(){
+    $('header').css('color','#FF0000');
+});
