28ebde29c14ef03600267ce5e32e5bc62c137c85
Mdlalose4life <blessing1059@gmail.com>
2023-05-25 04:59:32 +0200

    the old work

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,4 @@
 // Select the element to change the color of
 const header = document.querySelector('header');
-//uUpdates the color of the header element
+//Updates the color of the header element
 header.style.color = '#FF0000'
--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -2,4 +2,4 @@ $(function () {
     $('#red_header').click(function () {
       $('header').css('color', '#FF0000');
     });
-  });
+  })
